999f4b0503ef276b15e54988ec97c308852a2f90
chathula <chathula@different.com.au>
2019-09-19 00:32:17 +0530

    update message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res)  {
- res.send('hello world updated');
+ res.send('hello world');
 });
 
 app.listen(process.env.PORT || 3000);
