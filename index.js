fd2c726a054e359ad828d45a2b9c007596e929f1
chathula <chathula@different.com.au>
2019-09-19 17:47:57 +0530

    remove scripts and update the message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res)  {
- res.send('hello world updated');
+ res.send('hello world changed through Github Actions!!');
 });
 
 app.listen(process.env.PORT || 3000);
