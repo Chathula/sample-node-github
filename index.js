3c472f0c6e5bc59f264217cfcb19ad7b2357623e
chathula <chathula@different.com.au>
2019-09-19 23:11:41 +0530

    changed response text

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res)  {
- res.send('hello world!');
+ res.send('NodeJS App');
 });
 
 app.listen(process.env.PORT || 3000);
